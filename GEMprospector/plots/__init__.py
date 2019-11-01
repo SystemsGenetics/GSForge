0c1c73f689dd607b0b39865098ae37714fb562ad
Tyler Biggs <biggstd@gmail.com>
2019-11-01 10:41:11 -0700

    updated plots and attempted kde fix

--- GEMprospector/plots/__init__.py
+++ GEMprospector/plots/__init__.py
@@ -31,6 +31,8 @@ def np_sample_distributions(counts: np.ndarray, labels: np.ndarray = None):
     """
     Calculate and overlay kernel density estimates of the given count matrix on a per-sample basis.
 
+    If this gives a ValueError you may need to install statsmodels for a more robust kernel estimate.
+
     :param numpy.ndarray counts:  The count matrix to be displayed.
     :param numpy.ndarray labels: If provided the output density estimates will be colored by their
         label membership.
@@ -43,9 +45,6 @@ def np_sample_distributions(counts: np.ndarray, labels: np.ndarray = None):
     if labels is not None:
         label_set = list(np.unique(labels))
         colors = {label: color for label, color in zip(label_set, sns.color_palette(n_colors=len(label_set)))}
-        patches = [mpatches.Patch(color=color, label=label)
-                   for label, color in colors.items()]
-        plt.legend(handles=patches)
 
     for index, sample_row in tqdm(enumerate(counts), total=counts.shape[0]):
 
@@ -56,6 +55,10 @@ def np_sample_distributions(counts: np.ndarray, labels: np.ndarray = None):
 
         else:
             sns.kdeplot(sample_row, ax=ax, legend=False, shade=False, gridsize=250)
+    if labels is not None:
+        patches = [mpatches.Patch(color=color, label=label)
+                   for label, color in colors.items()]
+        plt.legend(handles=patches)
 
     return ax
 
