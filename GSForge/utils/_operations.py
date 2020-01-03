81f98a83266db8be02702f625b2219e0058f35e7
Tyler <biggstd@gmail.com>
2020-01-03 13:29:10 -0800

    fixed bug in mProbes

--- GSForge/utils/_operations.py
+++ GSForge/utils/_operations.py
@@ -7,3 +7,17 @@ def shuffle_along_axis(a, axis):
     """
     idx = np.random.rand(*a.shape).argsort(axis=axis)
     return np.take_along_axis(a, idx, axis=axis)
+
+
+def null_rank_distribution(real, shadow):
+    """
+    Returns the percent for which shadow values are ranked higher than a given real value.
+
+    :param real: The 'real' scores.
+
+    :param shadow: Scores from fake, or shadow, features.
+
+    :return: An array of the same shape as ``real``, with the percent of times a shadow
+      score had a higher value.
+    """
+    return np.sum(real[:, None] <= shadow[None, :], axis=-1) / shadow.shape[0]
