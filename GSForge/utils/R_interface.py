cf620d6d334c38ad6cf27f5661629f344eeac2bd
Tyler <biggstd@gmail.com>
2020-01-03 22:31:57 -0800

    updated docs

--- GSForge/utils/R_interface.py
+++ GSForge/utils/R_interface.py
@@ -1,25 +1,37 @@
 """
 This module contains functions for preparing data for transfer to and from the R programming language.
 
-This functionality is powered by the [`rpy2`](https://rpy2.readthedocs.io/en/version_2.8.x/) library.
+Such functionality is powered by the `rpy2 <https://rpy2.readthedocs.io/en/version_2.8.x/>`_ library.
 
-This means:
-+ The count and label dataframes will have the same indexes.
-+ The shape will be transposed (genes as rows and samples as columns).
+No conversion function is needed for preparing labels, as one can use the builtin ``pandas.DataFrame``
+function ``to_dataframe``.::
+
+    label_df = labels.to_dataframe()
+
 """
+
 import xarray as xr
 
 __all__ = [
     "Py_counts_to_R",
-    "Py_labels_to_R",
     "R_counts_to_Py_counts",
 ]
 
 
 def Py_counts_to_R(counts: xr.DataArray):
-    """Prepare a count `xarray.DataArray` as a `pandas.DataFrame` for transfer to the R programming language.
+    """
+    Prepare a count ``xarray.DataArray`` as a ``pandas.DataFrame`` for transfer to the R programming language.
 
-    Removes dashes from index names, as R does something(?) with them.
+    This function transposes the data to have genes as rows and samples as columns.
+    It then converts to a ``pandas.DataFrame`` and removes extraneous index levels.
+
+    The inverse of this function is ``R_counts_to_Py_counts``.
+
+    :param counts:
+        An ``xr.DataArray`` count matrix.
+
+    :returns count_df:
+        A ``pandas.DataFrame`` ready to transfer to an R environment.
     """
     # Get a copy and stack the data so it is no longer flat.
     count_df = counts.transpose().copy().to_dataframe().unstack()
@@ -29,15 +41,23 @@ def Py_counts_to_R(counts: xr.DataArray):
     return count_df
 
 
-def Py_labels_to_R(labels: xr.DataArray):
-    """
-    :param labels:
-    :return:
-    """
-    label_df = labels.copy().to_dataframe()
-    return label_df
-
-
 def R_counts_to_Py_counts(r_count_array, original_count_array):
+    """
+    Prepares a ``numpy`` array (count matrix) for use in ``GSForge``.
+
+    This function transposes the data (so that it has samples as rows and genes as columns).
+
+    Inverts the conversion provided by ``Py_counts_to_R``.
+
+    :param r_count_array:
+        A ``numpy`` array of count values. Presumed to be oriented with genes as rows,
+        and samples as columns.
+
+    :param original_count_array:
+        A copy of the original count array from which coordinates will be drawn.
+
+    :return:
+        An ``xarray.DataArray`` of the count values.
+    """
     values = r_count_array.transpose()
     return xr.DataArray(values, coords=original_count_array.coords)
