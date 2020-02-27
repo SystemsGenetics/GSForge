e423b7db5c18b33807d8404a54199c0753296a02
Tyler Biggs <biggstd@gmail.com>
2020-02-26 16:16:13 -0800

    updated docs, travis_dev

--- GSForge/__init__.py
+++ GSForge/__init__.py
@@ -1,24 +1,3 @@
-"""
-****************
-Reference Manual
-****************
-
-Welcome to the API reference for GSForge.
-
-`GSForge.models`_
- Primary data models for GSForge.
-`GSForge.operations`_
- Transforms and feature selection tools.
-`GSForge.panels`_
- Interactive visualizations.
-`GSForge.plots`_
- Plotting tools.
-`GSForge.utils`_
- Utilities.
-
-"""
-
-
 from .models import *
 from .operations.core import *
 
