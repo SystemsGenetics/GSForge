c57febd7b3262975d2c4e267aec59a29f0a20675
Tyler Biggs <biggstd@gmail.com>
2020-02-27 14:45:11 -0800

    removed extra section break from notebooks, travis_dev

--- GSForge/__init__.py
+++ GSForge/__init__.py
@@ -1,3 +1,22 @@
+"""
+Welcome to the GSForge API reference documentation.
+
+:ref:`GSForge.models`
+ Primary data models for GSForge.
+
+:ref:`GSForge.operations`
+ Transforms and feature selection tools.
+
+:ref:`GSForge.plots`
+ Plotting tools.
+
+:ref:`GSForge.panels`
+ Interactive visualizations.
+
+:ref:`GSForge.utils`
+ Utilities.
+"""
+
 from .models import *
 from .operations.core import *
 
