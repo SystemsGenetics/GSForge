6aae5635c6cec3137f68f780764e160c143c0134
Tyler Biggs <biggstd@gmail.com>
2020-03-09 11:33:15 -0700

    reduced python version requirement

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     author='Tyler Biggs',
     author_email='tyler.biggs@wsu.edu',
     description='Feature (gene) selection package for gene expression data.',
-    python_requires='>=3.8',
+    python_requires='>=3.6',
     install_requires=requirements,
     extras_require={
         'docs': [
