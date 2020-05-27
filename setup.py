ee26435c27864942825ac53b112ed01d222f4f4b
Tyler Biggs <biggstd@gmail.com>
2020-05-27 14:07:19 -0700

    changed nbsite version, travis_dev

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     install_requires=requirements,
     extras_require={
         'docs': [
-            'nbsite==0.6.7',
+            'nbsite',
             'nbsphinx',
             'selenium',
             'sphinx_ioam_theme',
