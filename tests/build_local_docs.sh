ff027cbd9144de6807e28240c4dd9eeb1ef783c1
Tyler Biggs <biggstd@gmail.com>
2021-02-01 11:36:56 -0800

    rc prep, travis_dev

---
+++ tests/build_local_docs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+OUTPUT_DIRECTORY="~/gsforge_local_docs"
+
+echo "generating module rst containers..."
+nbsite_generate_modules.py GSForge -d ./doc/Reference_Manual -n GSForge
+echo "generating notebook rst containers..."
+nbsite generate-rst --org SystemsGenetics --project-name GSForge
+echo "building docs..."
+nbsite build --what=html --output=builtdocs
+
